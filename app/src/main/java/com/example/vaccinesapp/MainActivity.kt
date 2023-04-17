0dcadeb35f08bc52a796c6092202d448687a1ce0
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-17 18:09:08 -0300

    Binding implemented

--- app/src/main/java/com/example/vaccinesapp/MainActivity.kt
+++ app/src/main/java/com/example/vaccinesapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.vaccinesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vaccinesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initialize()
+
+
+
+
+    }
+
+    private fun initialize(){
+        with(binding){
+
+        }
     }
 }
