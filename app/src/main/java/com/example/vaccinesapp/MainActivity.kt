6e30c574887c87a10536926fa196d2f2d55ae2f5
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-17 18:21:36 -0300

    Create vaccines list and implement the adapter

--- app/src/main/java/com/example/vaccinesapp/MainActivity.kt
+++ app/src/main/java/com/example/vaccinesapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.vaccinesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.vaccinesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun initialize(){
         with(binding){
+
+            //AdapterView: RecyclerView
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+
+            //List of VaccineModal Objects
             var vaccinesList : ArrayList<VaccineModel> = ArrayList()
+
+            val v1 = VaccineModel("Covid-19", R.drawable.vaccine1)
+            val v2 = VaccineModel("Hepatitis B", R.drawable.vaccine4)
+            val v3 = VaccineModel("Tetanus", R.drawable.vaccine5)
+            val v4 = VaccineModel("Pneumococcal", R.drawable.vaccine6)
+            val v5 = VaccineModel("Rotavirus", R.drawable.vaccine7)
+            val v6 = VaccineModel("Measles", R.drawable.vaccine8)
+            val v7 = VaccineModel("Chickenpox", R.drawable.vaccine9)
+
+            vaccinesList.add(v1)
+            vaccinesList.add(v2)
+            vaccinesList.add(v3)
+            vaccinesList.add(v4)
+            vaccinesList.add(v5)
+            vaccinesList.add(v6)
+            vaccinesList.add(v7)
+
+            //Adapter
+            val adapter = MyAdapter(vaccinesList)
+            recyclerView.adapter = adapter
+
         }
     }
 }
